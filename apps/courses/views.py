11b01ef6418445172dce9af829d4304270320b3a
Alejandro <alex.lopez.rubio2600@gmail.com>
2026-04-07 13:58:10 -0600

    feat: initialize Django project structure with core apps, models, and base templates

---
+++ apps/courses/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def course_list(request):
+    courses=[{
+        
+    }]
+    return render(request, "courses/courses.html")
+
+
+def course_detail(request):
+    pass
+
+def course_lessons(request):
+    pass
+
+
+
