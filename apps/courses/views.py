826ec734fffb02df58f7f182eca9bec25491a6e3
Alejandro <alex.lopez.rubio2600@gmail.com>
2026-04-07 15:39:17 -0600

    feat: implement course listing and detail views with corresponding templates

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,14 +2,96 @@ from django.shortcuts import render
 
 # Create your views here.
 def course_list(request):
-    courses=[{
-        
-    }]
-    return render(request, "courses/courses.html")
+    courses=[
+        {
+        'id': 1,
+        'level': 'Principiante',
+        'rating': 4.5,
+        'course_title': 'Introduccion a python',
+        'instructor': 'Alison Walsh',
+        'course_image': 'images/curso_1.jpg',
+        'instructor_image': 'https://randomuser.me/api/portraits/women/68.jpg',
+        },
+        {
+            'id': 2,
+            'level': 'Principiante',
+            'rating': 4.8,
+            'course_title': 'Introduccion a Django',
+            'instructor': 'Patty Kutch',
+            'course_image': 'images/curso_2.jpg',
+            'instructor_image': 'https://randomuser.me/api/portraits/women/20.jpg',
+        },
+        {
+            'id': 3,
+            'level': 'Avanzado',
+            'rating': 4.8,
+            'course_title': 'Django avanzado',
+            'instructor': 'Alonzo Murray',
+            'course_image': 'images/curso_3.jpg',
+            'instructor_image': 'https://randomuser.me/api/portraits/men/32.jpg',
+        },
+        {
+            'id': 4,
+            'level': 'Avanzado',
+            'rating': 4.7,
+            'course_title': 'Fast api avanzado',
+            'instructor': 'Gregory Harris',
+            'course_image': 'images/curso_4.jpg',
+            'instructor_image': 'https://randomuser.me/api/portraits/men/45.jpg',
+        }
+    ]
+    return render(request, "courses/courses.html", {'courses': courses})
 
 
 def course_detail(request):
-    pass
+    course ={
+        'course_title': 'Introduccion a python',
+        'course_link': '',
+        'info_course':{
+            'lessons': 79,
+            'duration': 8,
+            'instructor': 'Alex Lopez',
+        },
+        'course_content':[
+            {
+                'id':1,
+                'name': 'Introduccion al curso',
+                'lessons':[
+                    {
+                        'id':1,
+                        'name': '¿Qué aprenderás en este curso?',
+                        'type': 'video',
+                        'duration': '5:00',
+                    },
+                    {
+                        'id':2,
+                        'name': 'Cómo usar la plataforma',
+                        'type': 'file',
+                        'duration': '5:00',
+                    },
+                ]
+            },
+            {
+                'id':2,
+                'name': 'Fundamentos necesarios de Python',
+                'lessons':[
+                    {
+                        'id':1,
+                        'name': 'Variables y tipos de datos',
+                        'type': 'video',
+                        'duration': '5:00',
+                    },
+                    {
+                        'id':2,
+                        'name': 'Condicionales y bucles',
+                        'type': 'video',
+                        'duration': '5:00',
+                    },
+                ]
+            }
+        ]
+    }
+    return render(request, 'courses/course_detail.html', {'course': course})
 
 def course_lessons(request):
     pass
