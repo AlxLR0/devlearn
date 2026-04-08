da8332ca5c5aa157f271663fa9ff333836a05e30
Alejandro <alex.lopez.rubio2600@gmail.com>
2026-04-08 15:37:22 -0600

    feat: implement dashboard and course detail views with associated templates and URL routing

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -46,7 +46,8 @@ def course_list(request):
 def course_detail(request):
     course ={
         'course_title': 'Introduccion a python',
-        'course_link': '',
+        'course_link': 'course_lessons',
+        'course_image': 'images/curso_1.jpg',
         'info_course':{
             'lessons': 79,
             'duration': 8,
@@ -94,7 +95,54 @@ def course_detail(request):
     return render(request, 'courses/course_detail.html', {'course': course})
 
 def course_lessons(request):
-    pass
+    lesson ={
+        'course_title': 'Introduccion a python',
+        'progress': 30,
+        'course_content':[
+            {
+                'id':1,
+                'name': 'Introduccion al curso',
+                'total_lessons': 6,
+                'complete_lessons': 2,
+                'lessons':[
+                    {
+                        'id':1,
+                        'name': '¿Qué aprenderás en este curso?',
+                        'type': 'video',
+                        'duration': '5:00',
+                    },
+                    {
+                        'id':2,
+                        'name': 'Cómo usar la plataforma',
+                        'type': 'file',
+                        'duration': '5:00',
+                    },
+                ]
+            },
+            {
+                'id':2,
+                'name': 'Fundamentos necesarios de Python',
+                'total_lessons': 6,
+                'complete_lessons': 2,
+                'lessons':[
+                    {
+                        'id':1,
+                        'name': 'Variables y tipos de datos',
+                        'type': 'video',
+                        'duration': '5:00',
+                    },
+                    {
+                        'id':2,
+                        'name': 'Condicionales y bucles',
+                        'type': 'video',
+                        'duration': '5:00',
+                    },
+                ]
+            }
+            
+        ]
+    }
+    return render(request, 'courses/course_lessons.html', {'lesson': lesson})
 
 
 
